06c92ce3f8eef341790f3ecac1e318a7c5658634
Dominik Leuzinger <leuzingerdominik@gmail.com>
2021-10-21 09:13:30 +0200

    Add basic scoring script

--- src/models/score.py
+++ src/models/score.py
@@ -10,9 +10,19 @@ import logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger()
 
+import os
 import pandas as pd
 import numpy as np
 from joblib import load
 
 from dotenv import find_dotenv, load_dotenv
-load_dotenv(find_dotenv())
+load_dotenv(find_dotenv(os.getenv('PROJECT_DIR')))
+
+# %%
+MODEL_PATH = os.path.join(os.getenv('PROJECT_DIR'), 'bin', 'models', 'model.pkl')
+model = load(MODEL_PATH)
+type(model)
+# %%
+X = ["Das ist ein Test, ob dieser Text als Fake News klassifiziert wird", "Ein weiterer Test"]
+model.predict_proba(X)
+# %%
